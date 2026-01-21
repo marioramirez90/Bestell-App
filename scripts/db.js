d070b86692ebca6519c290da4eb91b63de429589
marioramirez90 <mario-ramirez@hotmail.de>
2026-01-21 13:03:31 +0100

    db: add object burger

---
+++ scripts/db.js
@@ -0,0 +1,26 @@
+let burger = [
+  {
+    name: "Veggie mushroom black burger",
+    description: "Fried green beans, tomatoes, lettuce, mushrooms",
+    price: 16.90,
+    image: "images/burger_veggie.png"
+  },
+  {
+    name: "All meat burger",
+    description: "Beef, bacon, beef patty, cheddar cheese",
+    price: 15.90,
+    image: "images/burger_all_meat.png"
+  },
+  {
+    name: "Beef red burger",
+    description: "Beef, cheese, tomatoes, sesame bread",
+    price: 14.90,
+    image: "images/burger_beef_red.png"
+  },
+  {
+    name: "Big chicken burger",
+    description: "Chicken fillet, tomatoes, lettuce, cheese, chili sauce",
+    price: 15.90,
+    image: "images/burger_big_chicken.png"
+  }
+];
